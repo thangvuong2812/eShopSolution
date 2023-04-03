81a0b5ab794690ee822ecb42987e4c1ff266a3ba
thangvuong2812 <thangvuong2812@gmail.com>
2023-04-03 21:09:56 +0700

    Create Application Layer

---
+++ eShopSolution.Application/Catalog/ManageProductService.cs
@@ -0,0 +1,52 @@
+﻿using DataAccess.Database;
+using DataAccess.Models;
+using eShopSolution.Application.Catalog.Products;
+using eShopSolution.Application.Catalog.Products.Dtos;
+using eShopSolution.Application.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShopSolution.Application.Catalog
+{
+    public class ManageProductService : IManageProductService
+    {
+        private readonly EShopDbContext _dbContext;
+        public ManageProductService(EShopDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task<int> Create(ProductCreateRequest productCreateRequest)
+        {
+            var product = new Product
+            {
+                Price = productCreateRequest.Price
+            };
+
+            _dbContext.Products.Add(product);
+            return await _dbContext.SaveChangesAsync();
+
+        }
+
+        public Task<int> Delete(long productId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<ProductViewModel>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PagedViewModel<ProductViewModel>> GetAllPaging(string keyword, int pageIndex, int pageSize)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> Update(ProductEditRequest productEditRequest)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ eShopSolution.Application/Catalog/PublicProductService.cs
@@ -0,0 +1,24 @@
+﻿using DataAccess.Database;
+using eShopSolution.Application.Catalog.Products;
+using eShopSolution.Application.Catalog.Products.Dtos;
+using eShopSolution.Application.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShopSolution.Application.Catalog
+{
+    public class PublicProductService : IPublicProductService
+    {
+        private readonly EShopDbContext _dbContext;
+        public PublicProductService(EShopDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public Task<PagedViewModel<ProductViewModel>> GetByCategoryId(int categoryId, int pageIndex, int pageSize)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
